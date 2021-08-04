c3e79f4c5536636625a20fb4957ded428d61f266
Doureyd <doureyd@gmail.com>
2021-08-04 13:31:51 +0200

    Add 404 Page

---
+++ src/pages/404.js
@@ -0,0 +1,21 @@
+import DefaultLayout from '@/layouts/DefaultLayout'
+import { ArrowBackIcon } from '@chakra-ui/icons'
+import { Button, Heading, Link } from '@chakra-ui/react'
+import NextLink from 'next/link'
+
+const FourOhFour = () => {
+  return (
+    <DefaultLayout>
+      <Heading as="h1" size="lg">
+        404 - Page Not Found
+      </Heading>
+      <NextLink href="/">
+        <Button mt="4" leftIcon={<ArrowBackIcon />} variant="outline">
+          Go back home
+        </Button>
+      </NextLink>
+    </DefaultLayout>
+  )
+}
+
+export default FourOhFour
--- src/pages/book-lists/[pid].js
+++ src/pages/book-lists/[pid].js
@@ -58,7 +58,7 @@ export async function getStaticPaths() {
 
   return {
     paths,
-    fallback: 'blocking',
+    fallback: false,
   }
 }
 
